c5fb9bb2bdb043ed0fe860e1d7a3bb8208183d2e
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2023-03-08 23:40:28 -0300

    feat: design tokens

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 09 Mar 2023 02:38:30 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorBlack": "#000000",
+  "ColorGray500": "#50505c",
+  "ColorGray700": "#1c1c1f",
+  "ColorGray800": "#18181b",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
